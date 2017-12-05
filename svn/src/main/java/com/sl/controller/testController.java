72470d6d89da5efaff057f09f6af04353da58563
夜来风雨声 <680707>
2017-12-05 19:18:04 +0800

    1111

---
+++ svn/src/main/java/com/sl/controller/testController.java
@@ -0,0 +1,14 @@
+package com.sl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @RequestMapping("test")
+    public String test(){
+      return  "第一次测试";
+    }
+}
